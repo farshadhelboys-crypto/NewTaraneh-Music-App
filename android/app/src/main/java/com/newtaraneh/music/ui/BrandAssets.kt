8581c9b1460f1becb683e65b01600374e02eee3e
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-09-19 17:54:51 +0330

    Fix brand image URLs for v2.0 CI build

--- android/app/src/main/java/com/newtaraneh/music/ui/BrandAssets.kt
+++ android/app/src/main/java/com/newtaraneh/music/ui/BrandAssets.kt
@@ -1,6 +1,6 @@
 package com.newtaraneh.music.ui
 
 object BrandAssets {
-    const val LOGO_URL = "https://h.uguu.se/CtOVgFoT.png"
-    const val BANNER_URL = "https://n.uguu.se/fmaLpHUT.jpg"
+    const val LOGO_URL = "https://d.uguu.se/RFmOWovL.png"
+    const val BANNER_URL = "https://h.uguu.se/zJnbVYrt.jpg"
 }
