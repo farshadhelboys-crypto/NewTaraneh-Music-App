71c5c6a8e353b4b4b57715514f0accd18672dd77
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-09-20 08:59:06 +0330

    v2.5: categories section with hashtag filters + covers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.newtaraneh.music"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "2.4.0"
+        versionCode = 25
+        versionName = "2.5.0"
     }
 
     buildTypes {
