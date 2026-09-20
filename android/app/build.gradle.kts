4041441e45822b3c77774140e88a5eb317e0ca1f
farshadhelboys-crypto <farshadhelboys@gmail.com>
2026-09-20 10:43:04 +0330

    Bump version to 2.6.1 (3D remix cover)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.newtaraneh.music"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "2.6.0"
+        versionCode = 27
+        versionName = "2.6.1"
     }
 
     signingConfigs {
